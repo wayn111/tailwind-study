830ecd96734d838b6f37b35a94350d12525655d0
wayn <1669738430@qq.com>
2022-08-23 00:14:21 +0800

    inti

---
+++ vite-tailwind/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <div class="h-screen w-full bg-red-400 flex justify-center items-center">
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  </div>
+</template>
+<style>
+</style>
